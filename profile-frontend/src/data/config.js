260f5765d63ba1dcb903ada70915f965f99983b6
AYUSH SARAF <ayushsaraf.saraf15@gmail.com>
2021-01-31 14:38:34 +0530

    make it better

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,9 +1,9 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Ashok Kumar",
-  greeting: "Hey 👋",
-  greetingDescription: "I'm Ashok Kumar and I'm a Software Engineer!",
+  name: "Ayush saraf",
+  greeting: "Bonjour 👋",
+  greetingDescription: "I'm Ayush saraf welcoming you all to my profile!",
   githubUrl: "https://github.com/ashokkumar95",
   linkedinUrl: "https://linkedin.com/ashokkumar1",
   cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
